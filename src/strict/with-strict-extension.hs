15efa6e00751bcc1941c15e10d23c99602c048e9
helq <linuxero789@gmail.com>
2019-10-14 12:04:40 -0500

    2019-10-14T12:04:40-05:00

---
+++ src/strict/with-strict-extension.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE Strict #-}
+
+module Main where
+
+import Debug.Trace (trace)
+import Data.Monoid ((<>))
+
+data D = D Int Int
+
+f :: Int -> D
+f 0 = D 0 undefined
+f i = D i (i `mod` 2)
+
+stupidShow :: D -> String
+stupidShow (D i _) = "D " <> show i
+
+topLevel :: Int
+topLevel = trace "evaluating toplevel" 10
+
+main :: IO ()
+main = do
+    let x = trace "building x" (f 0)
+        y = x `seq` trace "building y" (f 10)
+
+    putStrLn "Should have already failed!!"
+
+    putStrLn (stupidShow y)
+    putStrLn (stupidShow x)
